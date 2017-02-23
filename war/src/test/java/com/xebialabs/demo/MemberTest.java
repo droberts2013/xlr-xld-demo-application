5e22395d90c3f23741a2b49cce0bc3af68e5045f
Dave Roberts <droberts@xebialabs.com>
2017-02-23 02:54:28 +0000

    Add JUnit test

---
+++ war/src/test/java/com/xebialabs/demo/MemberTest.java
@@ -0,0 +1,35 @@
+package com.xebialabs.dave.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MemberTest {
+
+	private String name = "Dave";
+	private String email = "droberts@xebialabs.com";
+	private String phoneNumber = "866-656-4408";
+	private Member member;
+
+	@Before
+	public void create() {
+		member = new Member(name, email, phoneNumber);
+	}
+	
+	@Test
+	public void checkName() {
+		assertTrue(this.member.getName().equals(this.name));
+	}
+
+	@Test
+	public void checkEmail() {
+		assertTrue(this.member.getEmail().equals(this.email));
+	}
+
+	@Test
+	public void checkPhone() {
+		assertTrue(this.member.getPhoneNumber().equals(this.phoneNumber));
+	}
+}
+
